8dac45528083ca3406b9507de2d892ac8b55f4b8
Farabi <farabi.hassan@yale.edu>
2016-10-07 01:50:21 +0000

    Add nickname route

--- server.js
+++ server.js
@@ -5,4 +5,8 @@ app.get('/', function(request, response){
     response.send('Hello MGT-656! I am a Ninja.');
 });
 
+app.get('/nickname', function(request, response){
+    response.send('splendid-wolverine');
+});
+
 app.listen(process.env.PORT || 4000);
